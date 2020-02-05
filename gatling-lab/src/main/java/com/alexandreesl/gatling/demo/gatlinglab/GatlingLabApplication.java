5bb6665bbf17f835c7d8b402038bbf8f924c102b
Alexandre Eleutério Santos Lourenço <alexandreesl@gmail.com>
2020-02-05 20:13:44 -0300

    initial commit

---
+++ gatling-lab/src/main/java/com/alexandreesl/gatling/demo/gatlinglab/GatlingLabApplication.java
@@ -0,0 +1,13 @@
+package com.alexandreesl.gatling.demo.gatlinglab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatlingLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatlingLabApplication.class, args);
+	}
+
+}
---
+++ gatling-lab/src/test/java/com/alexandreesl/gatling/demo/gatlinglab/GatlingLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandreesl.gatling.demo.gatlinglab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatlingLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
